4652eb7f4e7974e30b055bf20c61794e8c831269
vijaybhadade <vijaybhadade01@gmail.com>
2026-09-09 23:02:42 +0530

    start dsa from today. let's back to study

--- Day1/first.java
+++ Day1/first.java
@@ -1,6 +1,32 @@
-public class first {
-    public static void main(String[] args) {
-      
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class first {
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            try{
+               
+                 System.out.println("Enter any number");
+            int n = sc.nextInt();
+           if(n > 0)
+           {
+            System.out.println("Given number is positive ");
+           }
+           else if(n < 0)
+           {
+            System.out.println("Given number is negative");
+           }
+           else {
+            System.out.println("Please enter valid number!");
+           }
+            }catch(InputMismatchException e)
+            {
+                System.out.println("Enter only interger values");
+            }
+           
+        }
     }
+
 }
